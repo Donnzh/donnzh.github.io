d7ea193c881abc1824af6eaef83b7bfc4476cddd
Donnzh <dongyang.tcm@gmail.com>
2016-07-04 11:08:27 +1000

    adding feature

--- js/main.js
+++ js/main.js
@@ -4,57 +4,67 @@ $window.on('scroll', check_if_in_view);
 $window.trigger('scroll');
 
 function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
 
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
+    $.each($animation_elements, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
 
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-          console.log('inview')
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
-    }
-  });
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            console.log('inview')
+            $element.addClass('in-view');
+        } else {
+            $element.removeClass('in-view');
+        }
+    });
 }
+jQuery(document).ready(function($) {
+    $('#slider').unslider();
+    $('#slider2').unslider();
 
+});
+
+$(function() {
+    $("#typed").typed({
+        strings: [" Hi! I am Donn, <br> <h4> <br>a Sydney based full-stack <span>web developer,<span></h4> <h4> passiniate in coding and love technologies.</h4>" ],
+        typeSpeed: 0,
+        showCursor: false
+    });
+
+ setTimeout(function(){
+    // $(".fadeIntext").css({
+    //   display:"inline"
+    // })
+
+    $(".fadeIntext").css('visibility','visible').hide().fadeIn(2000)
+  },4000);
+
+});
 
 // $(function(){
 //
 //   setInterval(function(){
 //     $('#slider .sliders').animate({'margin-left':'-=720px'},1000)
 //   })
-  //setInterval
-    //animate margin-left
-    //if its last slide, go position1
+//setInterval
+//animate margin-left
+//if its last slide, go position1
 
 // })
 
 
-jQuery(document).ready(function($) {
-			$('#slider').unslider();
-      $('#slider2').unslider();
-
-		});
 
 
 
-    // $(function(){
-    //        $("#typed").typed({
-    //            stringsElement: $('#typeElement')
-    //        });
-    //    });
 
-       $(function(){
-$("#typed").typed({
-strings: [" Hello, I am Donn, here is my intorduction hk.", "Second sentence."],
-typeSpeed: 10
-});
-});
+// $(function(){
+//        $("#typed").typed({
+//            stringsElement: $('#typeElement')
+//        });
+//    });
